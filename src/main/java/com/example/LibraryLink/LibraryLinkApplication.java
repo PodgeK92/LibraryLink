24c0b33b02481b55a03773656b4e5cb18587014b
podgek92 <p.keane14@universityofgalway.ie>
2024-04-14 21:31:26 +0100

    Initial commit

---
+++ src/main/java/com/example/LibraryLink/LibraryLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLinkApplication.class, args);
+	}
+
+}
